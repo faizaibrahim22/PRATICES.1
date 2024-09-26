c2be51f25d6e684899e5db443c06b628a166d0ed
faizaibrahim22 <faizaibrahim14789@gmail.com>
2024-09-27 00:17:12 +0500

    next.js

---
+++ component/Card.tsx
@@ -0,0 +1,18 @@
+//// what is components
+/// components first letter caplita hona chaya
+///  components parent elemts return karta hai ?
+///  components mai hum html css typescript or javascirtp use karta hai
+///   components html ka andar hum javascript or typescrip use karta hai    ?
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+const Card =()=>{
+    return (
+    <div>
+        <h3> my first elements </h3>
+    </div>
+        
+    )
+};
+
+export default Card;
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+import Card from '../../component/Card';
+
+export default function Home() {
+  return (
+      <div>
+      <b> hello faiza</b>
+        <Card/>
+      </div>
+  )
+
+};
